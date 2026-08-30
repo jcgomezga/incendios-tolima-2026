7b004dd6aefc0361c3824d2a856ea6241c2ab029
jcgomezga <juancarlos.ota2@gmail.com>
2026-08-29 19:05:44 -0500

    Agregar A0 web optimizados y descargas originales

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -3,21 +3,24 @@ window.SITE_CONFIG = {
     anm: {
       label: "ANM",
       a3: "assets/maps/anm_a3.jpg",
-      a0: "assets/maps/anm_a0.jpg",
+      a0: "assets/maps/anm_a0_web.jpg",
+      a0Original: "https://github.com/jcgomezga/incendios-tolima-2026/releases/latest/download/anm_a0_original.jpg",
       titleA3: "ANM · mapa A3",
       titleA0: "ANM · mapa A0 de exploración detallada"
     },
     anla: {
       label: "ANLA",
       a3: "assets/maps/anla_a3.jpg",
-      a0: "assets/maps/anla_a0.jpg",
+      a0: "assets/maps/anla_a0_web.jpg",
+      a0Original: "https://github.com/jcgomezga/incendios-tolima-2026/releases/latest/download/anla_a0_original.jpg",
       titleA3: "ANLA · mapa A3",
       titleA0: "ANLA · mapa A0 de exploración detallada"
     },
     anh: {
       label: "ANH",
       a3: "assets/maps/anh_a3.jpg",
-      a0: "assets/maps/anh_a0.jpg",
+      a0: "assets/maps/anh_a0_web.jpg",
+      a0Original: "https://github.com/jcgomezga/incendios-tolima-2026/releases/latest/download/anh_a0_original.jpg",
       titleA3: "ANH · mapa A3",
       titleA0: "ANH · mapa A0 de exploración detallada"
     }
